e996533e6e730aeb8ee8db95655234972c6f0fe5
Dan Yamamoto <dan@iij.ad.jp>
2021-07-07 09:58:10 +0000

    test: add test for TermwiseStatement

--- __tests__/expVC.spec.ts
+++ __tests__/expVC.spec.ts
@@ -8,6 +8,8 @@ import {
 import {
   BbsBlsSignature2020,
   BbsBlsSignatureProof2020,
+  BbsBlsSignatureProofTermwise2020,
+  BbsBlsSignatureTermwise2020,
   Bls12381G2KeyPair
 } from "../src/index";
 import { signDeriveVerify } from "./utils";
@@ -15,15 +17,29 @@ import { signDeriveVerify } from "./utils";
 const expKey = new Bls12381G2KeyPair(expExampleBls12381KeyPair);
 
 describe("experimental verifiable credentials", () => {
-  it("should sign, derive proof, and verify proof on experimental verifiable credential", async () => {
+  it("[StringStatement] should sign, derive proof, and verify proof on experimental verifiable credential", async () => {
+    const vc = { ...expVCDocument };
     await signDeriveVerify(
-      expVCDocument,
+      vc,
       expRevealDocument,
       {},
       expKey,
       expCustomLoader,
       BbsBlsSignature2020,
       BbsBlsSignatureProof2020
+    );
+  });
+
+  it("[TermwiseStatement] should sign, derive proof, and verify proof on experimental verifiable credential", async () => {
+    const vc = { ...expVCDocument };
+    await signDeriveVerify(
+      vc,
+      expRevealDocument,
+      {},
+      expKey,
+      expCustomLoader,
+      BbsBlsSignatureTermwise2020,
+      BbsBlsSignatureProofTermwise2020
     );
   });
 });
